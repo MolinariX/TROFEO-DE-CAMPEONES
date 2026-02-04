60200d7d794922d6cccdc1078837cd379dee20a0
MolinariX <juanmolinarii@gmail.com>
2026-02-04 12:55:14 -0300

    Se actualizó la tabla de Tarjetas Acumuladas

--- script.js
+++ script.js
@@ -142,7 +142,8 @@ const FALLBACK_IMG = "https://placehold.co/100x100/111/e50914?text=FC";
                             ],
                             [
                                 { player: "Javier Bilicich", type: "roja" },
-                                { player: "Tomás Quinteros", type: "amarilla" },
+                                { player: "Juan Molinari", type: "amarilla" },
+                                { player: "Samuel Garay", type: "amarilla" },
                                 { player: "Federico Martínez", type: "roja" }
                             ]
                         ) 
@@ -293,15 +294,18 @@ const FALLBACK_IMG = "https://placehold.co/100x100/111/e50914?text=FC";
 
         // ================= JUGADORES =================
         const playersData = [
-            //{ name: "Alexis Silva", teamId: "pacifico", goals: 0, yellow: 0, red: 2, img: "imagen/jugadores/alexis_silva.png", suspensionMatches: 0 },
+            { name: "Alexis Silva", teamId: "pacifico", goals: 0, yellow: 3, red: 2, img: "imagen/jugadores/alexis_silva.png", suspensionMatches: 0 },
+            { name: "Tomás Quinteros", teamId: "pacifico", goals: 0, yellow: 3, red: 0, img: "imagen/jugadores/tomas_quinteros.png" },
+            { name: "Cristian Dulcich", teamId: "pacifico", goals: 0, yellow: 3, red: 0, img: "imagen/jugadores/cristian_dulcich.png", suspensionMatches: 0 },
+            { name: "Nicolás Miño", teamId: "pacifico", goals: 0, yellow: 2, red: 0, img: "imagen/jugadores/nicolas_mino.png", suspensionMatches: 0 },
             { name: "Juan Molinari", teamId: "pacifico", goals: 1, yellow: 1, red: 0, img: "imagen/jugadores/juan_molinari.png", suspensionMatches: 0 },
             { name: "Samuel Garay", teamId: "pacifico", goals: 2, yellow: 1, red: 0, img: "imagen/jugadores/samuel_garay.png", suspensionMatches: 0 },
-            { name: "Nicolás Miño", teamId: "pacifico", goals: 0, yellow: 0, red: 0, img: "imagen/jugadores/nicolas_mino.png", suspensionMatches: 0 },
-            //{ name: "Gabriel Romanutti", teamId: "pacifico", goals: 0, yellow: 0, red: 1, img: "imagen/jugadores/gabi_romanutti.png", suspensionMatches: 0 },
-            { name: "Federico Martinez", teamId: "pacifico", goals: 1, yellow: 0, red: 1, img: "imagen/jugadores/federico_martinez.png",suspensionMatches: 1 },
+            { name: "Gabriel Romanutti", teamId: "pacifico", goals: 0, yellow: 1, red: 1, img: "imagen/jugadores/gabi_romanutti.png", suspensionMatches: 0 },
+            { name: "Federico Martinez", teamId: "pacifico", goals: 1, yellow: 1, red: 1, img: "imagen/jugadores/federico_martinez.png",suspensionMatches: 1 },
             { name: "Javier Bilicich", teamId: "pacifico", goals: 0, yellow: 0, red: 1, img: "imagen/jugadores/javier_bilicich.png", suspensionMatches: 2 },
+            { name: "Agustín Alvarez", teamId: "pacifico", goals: 0, yellow: 1, red: 0, img: "imagen/jugadores/agustin_alvarez.png", suspensionMatches: 0 },
+            { name: "Tobias Fresco", teamId: "pacifico", goals: 0, yellow: 1, red: 0, img: "imagen/jugadores/tobias_fresco.png", suspensionMatches: 0 },
             { name: "Santiago Quinteros", teamId: "pacifico", goals: 0, yellow: 1, red: 0, img: "imagen/jugadores/santiago_quinteros.png" },
-            { name: "Tomás Quinteros", teamId: "pacifico", goals: 0, yellow: 2, red: 0, img: "imagen/jugadores/tomas_quinteros.png" },
             // Ejemplos añadidos para demostración de las nuevas tablas
            // { name: "Lucas Pérez", teamId: "murphy", goals: 1, yellow: 3, red: 0, img: "imagen/jugadores/default.png", suspensionMatches: 1 },
             { name: "Bernardo Giordano", teamId: "pacifico", goals: 0, yellow: 0, red: 0, img: "imagen/jugadores/bernardo_giordano.png", injuryStatus: "Desgarro Muscular"  }
