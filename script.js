315421a9db9b41bb262528fbd89f85680497f107
MolinariX <juanmolinarii@gmail.com>
2026-03-07 17:56:03 -0300

    Actualizacion

--- script.js
+++ script.js
@@ -239,8 +239,10 @@ const FALLBACK_IMG = "https://placehold.co/100x100/111/e50914?text=FC";
                     { home: 'Sportivo Peñarol', away: 'Los Resa', court: 2, turn: 1, date: '07/03', time: '15:00', homeScore: 0, awayScore: 0, played: false },
                     { 
                         home: 'Flamingo F.C', away: 'Pacífico F.C', court: 3, turn: 1, date: '07/03', time: '15:00', 
-                        homeScore: 0, awayScore: 0, played: false,
-                        details: createPacificoDetails([], []) 
+                        homeScore: 2, awayScore: 2, played: true,
+                        details: createPacificoDetails([
+                            {player: " Samuel Garay ", goals: 1 },
+                            {player: "Gabriel Romanutti ", goals: 1 }], []) 
                     },
                     { home: 'Murphy F.C', away: 'Schmol Alineaciones', court: 4, turn: 1, date: '07/03', time: '15:00', homeScore: 0, awayScore: 0, played: false }
                 ]
@@ -310,20 +312,21 @@ const FALLBACK_IMG = "https://placehold.co/100x100/111/e50914?text=FC";
 
         // ================= JUGADORES =================
         const playersData = [
-            { name: "Santiago Quinteros", teamId: "pacifico", goals: 0, yellow: 3, red: 0, img: "imagen/jugadores/santiago_quinteros.png" },
+            { name: "Santiago Quinteros", teamId: "pacifico", goals: 0, yellow: 3, red: 0, img: "imagen/jugadores/santiago_quinteros.png",injuryStatus: "Lesión rodilla" },
             { name: "Cristian Dulcich", teamId: "pacifico", goals: 0, yellow: 3, red: 0, img: "imagen/jugadores/cristian_dulcich.png", suspensionMatches: 0 },
             { name: "Nicolás Miño", teamId: "pacifico", goals: 0, yellow: 2, red: 0, img: "imagen/jugadores/nicolas_mino.png", suspensionMatches: 0 },
-            { name: "Gabriel Romanutti", teamId: "pacifico", goals: 1, yellow: 2, red: 1, img: "imagen/jugadores/gabi_romanutti.png", suspensionMatches: 0 },
+            { name: "Gabriel Romanutti", teamId: "pacifico", goals: 2, yellow: 2, red: 1, img: "imagen/jugadores/gabi_romanutti.png", suspensionMatches: 0 },
             { name: "Federico Martinez", teamId: "pacifico", goals: 1, yellow: 1, red: 1, img: "imagen/jugadores/federico_martinez.png",suspensionMatches: 0 },
             { name: "Javier Bilicich", teamId: "pacifico", goals: 0, yellow: 1, red: 1, img: "imagen/jugadores/javier_bilicich.png", suspensionMatches: 0 },
             { name: "Juan Molinari", teamId: "pacifico", goals: 1, yellow: 1, red: 0, img: "imagen/jugadores/juan_molinari.png", suspensionMatches: 0, injuryStatus: "Desgarro Muscular" },
-            { name: "Samuel Garay", teamId: "pacifico", goals: 4, yellow: 1, red: 0, img: "imagen/jugadores/samuel_garay.png", suspensionMatches: 0 },
+            { name: "Samuel Garay", teamId: "pacifico", goals: 5, yellow: 1, red: 0, img: "imagen/jugadores/samuel_garay.png", suspensionMatches: 0 },
             { name: "Tobias Fresco", teamId: "pacifico", goals: 0, yellow: 1, red: 0, img: "imagen/jugadores/tobias_fresco.png", suspensionMatches: 0 },
-            { name: "Tomás Quinteros", teamId: "pacifico", goals: 0, yellow: 0, red: 0, img: "imagen/jugadores/tomas_quinteros.png", suspensionMatches: 0 },
+            { name: "Tomás Quinteros", teamId: "pacifico", goals: 0, yellow: 0, red: 1, img: "imagen/jugadores/tomas_quinteros.png", suspensionMatches: 1 },
             { name: "Manuel Camino", teamId: "pacifico", goals: 1, yellow: 0, red: 0, img: "imagen/jugadores/manuel_camino.png", suspensionMatches: 0 },
             // Ejemplos añadidos para demostración de las nuevas tablas
            // { name: "Lucas Pérez", teamId: "murphy", goals: 1, yellow: 3, red: 0, img: "imagen/jugadores/default.png", suspensionMatches: 1 },
-            { name: "Bernardo Giordano", teamId: "pacifico", goals: 0, yellow: 0, red: 0, img: "imagen/jugadores/bernardo_giordano.png", injuryStatus: false  }
+            { name: "Bernardo Giordano", teamId: "pacifico", goals: 0, yellow: 0, red: 0, img: "imagen/jugadores/bernardo_giordano.png", injuryStatus: false  },
+            { name: "Nicolas Lucero", teamId: "pacifico", goals: 0, yellow: 0, red: 0, img: "imagen/jugadores/nicolas_lucero.png", injuryStatus: "Posible desgarro muscular"  }
 
         ];
 
